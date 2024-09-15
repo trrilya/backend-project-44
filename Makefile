231d1a2b7a6f0e4195b7256e40a0c67224edd677
Titov Ilya <trrkarpov@gmail.com>
2024-09-15 13:57:55 +0500

    update files: Makefile, bin/brain-games.js

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 install:
         npm ci
 brain-games:
-        node bin/brain-games.js
+        ./bin/brain-games.js
 publish:
         npm publish --dry-run
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!');
+
+import _ from "readline-sync";
+
+var readlineSync = require('readline-sync');
+
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hello,' + userName + '!');
