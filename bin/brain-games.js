6d08621d342ffc995f5c00251664b2d47ca760d4
Titov Ilya <trrkarpov@gmail.com>
2024-09-16 13:29:20 +0500

    update bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!');
 
-import _ from "readline-sync";
-
-var readlineSync = require('readline-sync');
+import readlineSync from "readline-sync";
 
 var userName = readlineSync.question('May I have your name? ');
 console.log('Hello,' + userName + '!');
