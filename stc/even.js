bf36b61d51c395dce94e90e418d4c9a86707f711
Titov Ilya <trrkarpov@gmail.com>
2024-10-01 22:12:53 +0500

    update even.js

--- stc/even.js
+++ stc/even.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+
+const answers = () => {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    const number = readlineSync.question('Question: ');
+    console.log('Hello, ' + userName + '!');
+};
