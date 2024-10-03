30cbf10c4d036c42165c4e8b7c333eb7459a1d58
Titov Ilya <trrkarpov@gmail.com>
2024-10-03 14:00:12 +0500

    update files

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 install:
 	npm ci
 brain-games:
-	node ./bin/brain-games.js
 	node ./bin/brain-even.js
 publish:
 	npm publish --dry-run
--- stc/even.js
+++ stc/even.js
@@ -1,16 +1,44 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import { random } from 'lodash';
 
 const answers = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    console.log('Welcome to the Brain Games!');
+
+    const userName = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + userName + '!');
     
-    function randomInteger(min, max) {
-        let rand = min + Math.random() * (max + 1 - min);
-        return Math.floor(rand);
-      };
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
       
-    const UserAnswer = readlineSync.question(`Question: ${randomInteger(0, 1000)} `);
+      const num = random(0, 1000);
+    const UserAnswer = readlineSync.question(`Question: ${num} `);
     console.log('Your answer: ' + UserAnswer);
+     const isEvenCheck = (answer, number) => {  
+     let isEven
+      if (number % 2 === 0) {
+        isEven = 'yes';
+      } else {
+        isEven = 'no';
+      }
+      if (answer === isEven) {
+        return 'Correct!'
+      } else {
+        return `'${answer}' is wrong answer ;(. Correct answer was '${isEven}'.
+Let's try again, ${userName}`
+      };
+    };
+    console.log(isEvenCheck(UserAnswer, num));
+    
+    const num2 = random(0, 1000);
+    const UserAnswer2 = readlineSync.question(`Question: ${num2} `);
+    console.log('Your answer: ' + UserAnswer2);
+    console.log(isEvenCheck(UserAnswer2, num2));
+
+    const num3 = random(0, 1000);
+    const UserAnswer3 = readlineSync.question(`Question: ${num3} `);
+    console.log('Your answer: ' + UserAnswer3);
+    console.log(isEvenCheck(UserAnswer3, num3));
+
 };
 
 export default answers;
