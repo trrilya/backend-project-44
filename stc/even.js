fade33d25d90750aa992cad4b9b5f64fa582474a
Titov Ilya <trrkarpov@gmail.com>
2024-10-09 13:03:50 +0500

    update, delete files

--- stc/even.js
+++ stc/even.js
@@ -2,43 +2,32 @@
 import readlineSync from 'readline-sync';
 import _ from 'lodash'; 
 const { random } = _; 
-const answers = () => {
-    console.log('Welcome to the Brain Games!');
 
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
-    
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-      
-      const num = random(0, 1000);
-    const UserAnswer = readlineSync.question(`Question: ${num} `);
-    console.log('Your answer: ' + UserAnswer);
-     const isEvenCheck = (answer, number) => {  
-     let isEven
-      if (number % 2 === 0) {
-        isEven = 'yes';
-      } else {
-        isEven = 'no';
-      }
-      if (answer === isEven) {
-        return 'Correct!'
-      } else {
-        console.error(`'${answer}' is wrong answer ;(. Correct answer was '${isEven}'.
-Let's try again, ${userName}`); 
-      };
-    };
-    console.log(isEvenCheck(UserAnswer, num));
-    
-    const num2 = random(0, 1000);
-    const UserAnswer2 = readlineSync.question(`Question: ${num2} `);
-    console.log('Your answer: ' + UserAnswer2);
-    console.log(isEvenCheck(UserAnswer2, num2));
-
-    const num3 = random(0, 1000);
-    const UserAnswer3 = readlineSync.question(`Question: ${num3} `);
-    console.log('Your answer: ' + UserAnswer3);
-    console.log(isEvenCheck(UserAnswer3, num3));
-
+const welcome = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log('Hello, ' + userName + '!');
+  return userName;
 };
 
-export default answers;
+const gameLoop = (userName, counter = 0) => {
+  if (counter === 3) {
+    console.log(`Congratulations, ${userName}!`);
+    return;
+  }
+
+  const num = random(0, 1000);
+  const userAnswer = readlineSync.question(`Question: ${num} `);
+  const isEven = num % 2 === 0 ? 'yes' : 'no';
+
+  if (userAnswer === isEven) {
+    console.log('Correct!');
+    gameLoop(userName, counter + 1);
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven}'.`);
+    console.log(`Let's try again, ${userName}!`);
+    return; 
+  }
+};
+
+export { welcome, gameLoop };
