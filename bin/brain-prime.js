51ce90b4af7ee957ba517d84e9057010b4ad517a
Titov Ilya <trrkarpov@gmail.com>
2024-11-04 12:50:13 +0500

    new files:   bin/brain-prime.js new,  games/prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import runGame from '../src/logic.js';
+import primeGame from '../games/prime.js';
+
+runGame(primeGame);
---
+++ games/prime.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import _ from 'lodash';
+const { random } = _;
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const generateRound = () => {
+  const num = random(1, 100);
+  function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return 'yes';
+    }
+    return 'no';
+  };
+
+  const question = `${num}`;
+  const correctAnswer = isPrime(num);
+
+  return { question, correctAnswer };
+};
+
+export default { description, generateRound };
