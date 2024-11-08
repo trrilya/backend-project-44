432bd374ef5ea2f11c8f94667c5d1560a405f374
Titov Ilya <trrkarpov@gmail.com>
2024-11-08 11:49:48 +0500

    rename eslint.config.js

---
+++ eslint.config.cjs
@@ -0,0 +1,23 @@
+const globals = require('globals');
+const pluginJs = require('@eslint/js');
+
+module.exports = [
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+    plugins: {
+      js: pluginJs,
+    },
+    rules: {
+      'no-trailing-spaces': 'error',
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'eol-last': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      'no-extra-semi': 'error',
+    },
+  },
+  pluginJs.configs.recommended,
+];
+
