dbe22df7fbc2ffb2d89c0042a90ce67bc2a9d73a
Titov Ilya <trrkarpov@gmail.com>
2024-11-10 14:50:39 +0500

    update files

--- games/prime.js
+++ games/prime.js
@@ -7,7 +7,7 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const generateRound = () => {
   const num = random(1, 100);
-  
+
   function isPrime(n) {
     if (n <= 1) return 'no';
     for (let i = 2; i <= Math.sqrt(n); i += 1) {
